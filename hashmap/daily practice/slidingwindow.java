794515b03c9f6ae193e8cb0ce48161e5cafba3ba
Pratham Sen <prathamsen2004@gmail.com>
2026-06-20 22:01:13 +0530

    Add solution for Fizz Buzz (LeetCode 412)

--- hashmap/daily practice/slidingwindow.java
+++ hashmap/daily practice/slidingwindow.java
@@ -456,6 +456,8 @@ public class slidingwindow{
 
  */
 
+/*
+
 import java.util.*;
 
 public class slidingwindow {
@@ -486,3 +488,35 @@ public class slidingwindow {
         System.out.println(ans);
     }
 }
+
+*/
+
+
+
+
+import java.util.*;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } 
+            else if (i % 3 == 0) {
+                result.add("Fizz");
+            } 
+            else if (i % 5 == 0) {
+                result.add("Buzz");
+            } 
+            else {
+                result.add(String.valueOf(i));
+            }
+        }
+
+        return result;
+    }
+}
+
