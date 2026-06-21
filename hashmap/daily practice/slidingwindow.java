47b8fd71a916ad7c2a54c3611e946dff2176e0b4
Pratham Sen <prathamsen2004@gmail.com>
2026-06-21 23:46:22 +0530

    Add valid parentheses solution

--- hashmap/daily practice/slidingwindow.java
+++ hashmap/daily practice/slidingwindow.java
@@ -491,7 +491,7 @@ public class slidingwindow {
 
 */
 
-
+/* 
 
 
 import java.util.*;
@@ -520,3 +520,45 @@ class Solution {
     }
 }
 
+*/
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class slidingwindow {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter brackets string: ");
+        String s = sc.nextLine();
+
+        System.out.println(isValid(s));
+
+        sc.close();
+    }
+}
